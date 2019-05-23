2f121dd8506a0d65a48a2c0861624920914edf16
stanley-c-so <stanley.c.so@gmail.com>
2019-05-23 12:29:49 -0400

    first commit

---
+++ mergesort.js
@@ -0,0 +1,57 @@
+function split(wholeArray) {
+    
+    // RECURSIVE SOLUTION:
+
+    // const midPoint = Math.ceil(wholeArray.length / 2);
+    // const firstHalf = wholeArray.slice(0, midPoint);
+    // const secondHalf = wholeArray.slice(midPoint);
+    // return [firstHalf, secondHalf];
+
+    // ITERATIVE SOLUTION:
+
+    const result = [];
+    wholeArray.forEach(element => result.push([element]));
+    return result;
+}
+
+function merge(firstHalf, secondHalf) {
+    let combinedArray = [];
+    const targetLength = firstHalf.length + secondHalf.length
+    while (combinedArray.length !== targetLength) {
+        if (firstHalf.length === 0) {
+            combinedArray = combinedArray.concat(secondHalf);
+        } else if (secondHalf.length === 0) {
+            combinedArray = combinedArray.concat(firstHalf);
+        } else if (firstHalf[0] < secondHalf[0]) {
+            combinedArray.push(firstHalf.splice(0, 1)[0]);
+        } else {
+            combinedArray.push(secondHalf.splice(0, 1)[0]);
+        }
+    }
+    return combinedArray;
+}
+
+function mergeSort(array) {
+    let mergedArray = [];
+    array = split(array);
+    while (array.length > 1) {
+        for (let i = 0; i < array.length; i += 2) {
+            if (i === array.length - 1) {
+                mergedArray.push(array[i])
+            } else {
+                mergedArray.push(merge(array[i], array[i + 1]));
+            }
+        }
+        array = mergedArray;
+        mergedArray = [];
+    }
+    return array[0];
+}
+
+
+// TESTS
+
+let a = [3, 9, 10, 27, 38, 43, 82]
+let b = [15, 35, 50, 75, 100, 105]
+
+console.log(mergeSort([...a, ...b]));
